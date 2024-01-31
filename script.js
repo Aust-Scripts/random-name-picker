8aa402214359fe223fd6a409dcb0d259458955ac
Austin <103972153+Samichpower@users.noreply.github.com>
2024-01-30 20:04:22 -0600

    Append random name to the page

--- script.js
+++ script.js
@@ -6,22 +6,21 @@ document.getElementById('name-form').addEventListener('submit', (event) => {
 
   const name = document.getElementById('name');
   list.push(name.value);
-  console.log(list);
 
   listContainer.innerHTML = '';
+  name.value = '';
 
   for (let i = list.length - 1; i >= 0; i--) {
     const newListItem = document.createElement('li');
     newListItem.textContent = list[i];
     listContainer.appendChild(newListItem);
-  }
-
-  name.value = '';
+  };
 });
 
 document.getElementById('random-name-button').addEventListener('click', () => {
   const randomNum = Math.floor(Math.random() * list.length);
+  const nameContainer = document.getElementById('random-name');
 
-  console.log(randomNum);
-  console.log(list[randomNum]);
+  nameContainer.innerHTML = '';
+  nameContainer.textContent = list[randomNum];
 })
